2c834751cff6b4cd3c1494935ba95252f844c6ad
ccastleberry <castle.caleb@gmail.com>
2020-04-23 17:08:21 -0400

    added DateLinear class and fixed the docstring and transform function in the DateFourier class

--- skautobots/date_processing/_date_fourier.py
+++ skautobots/date_processing/_date_fourier.py
@@ -14,9 +14,11 @@ def fourier_cos_encoding(series, max_val, n=1):
 
 
 class DateFourier(BaseEstimator, TransformerMixin):
-    """ A transformer that .
-    For more information regarding how to build your own transformer, read more
-    in the :ref:`User Guide <user_guide>`.
+    """ A transformer that adds fourier terms for a given date period.
+
+    For each passed in date column this transformer will compute
+    a number of columns for the fourier series based on the passed in
+    periods.
 
     Parameters
     ----------
@@ -115,16 +117,16 @@ class DateFourier(BaseEstimator, TransformerMixin):
         for col in self.cols:
             cyclical_cols = {}
             if 'dayofweek' in self.periods:
-                df['{}_day_of_week'.format(col)] = df['load_date'].dt.dayofweek
+                df['{}_day_of_week'.format(col)] = df[col].dt.dayofweek
                 cyclical_cols['{}_day_of_week'.format(col)] = 7
             if 'dayofmonth' in self.periods:
-                df['{}_day_of_month'.format(col)] = df['load_date'].dt.day
+                df['{}_day_of_month'.format(col)] = df[col].dt.day
                 cyclical_cols['{}_day_of_month'.format(col)] = 31
             if 'dayofyear' in self.periods:
-                df['{}_day_of_year'.format(col)] = df['load_date'].dt.dayofyear
+                df['{}_day_of_year'.format(col)] = df[col].dt.dayofyear
                 cyclical_cols['{}_day_of_year'.format(col)] = 365
             if 'monthofyear' in self.periods:
-                df['{}_month'.format(col)] = df['load_date'].dt.month
+                df['{}_month'.format(col)] = df[col].dt.month
                 cyclical_cols['{}_month'.format(col)] = 12
 
             for fcol, max_val in cyclical_cols.items():
